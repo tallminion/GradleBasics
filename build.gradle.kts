f8c55fd87c2c66d7211e4476d2915414bc502523
tallminion <sagarkr1109@gmail.com>
2024-07-12 00:47:41 +0530

    New gradle project setup done

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+            "Main-Class" to "org.example.Main"  // Replace with your actual main class
+        )
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
