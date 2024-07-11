80a2bea630bef0bce72c46d483e20e73ba8a7215
tallminion <sagarkr1109@gmail.com>
2024-07-12 01:49:01 +0530

    Added okHttp to make http requests from plain java code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,26 @@
 package org.example;
 
+import okhttp3.OkHttp;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        for (int i = 1; i <= 5; i++) System.out.println("i = " + i);
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://fakestoreapi.com/products/1";
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) System.out.println("something went wrong!!!");
+            System.out.println(response.body().string());
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
     }
 }
