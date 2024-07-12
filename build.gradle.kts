e1ee8c6015c9b4ff27940f1520645ee1a5a486ae
tallminion <sagarkr1109@gmail.com>
2024-07-12 12:39:13 +0530

    added gson dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.google.code.gson:gson:2.8.9")
 }
 
 tasks.jar {
